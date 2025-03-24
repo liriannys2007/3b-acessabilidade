8d7f7aaffb5ac5ec08cc5d66b568434239c800c0
Liriannys19 <141455405+liriannys2007@users.noreply.github.com>
2025-03-24 08:22:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesAcessibili = document.getElementById('apresenta-lista');
+
+botaoAcessibilidade.addEventListener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-menu')
+        opcoesAcessibili.classList.toggle('apresenta-lista')
+})
+
 const aumentarFonteBotao = document.getElementByI('aumentar-fonte');
 const diminuirFonteBotao = document.getElementByI('diminuir-fonte');
 
@@ -11,4 +20,7 @@ aumentarFonteBotao.addEventListener ('click', function()    {
 diminuirFonteBotao.addEventListener ('click', function()    {
             tamnhoFonteAtual += 0.1;
             document.body.style.fontSize = '${tamanhoFonteAtual} rem'
+})
+
+
 })
